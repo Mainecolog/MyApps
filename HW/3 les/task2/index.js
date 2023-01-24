1bb2d73ace956e8d0c714cf06a1339ad7c1eabf5
Mainecolog <123167632+Mainecolog@users.noreply.github.com>
2023-01-24 11:20:47 +0300

    Les 3 DONE

---
+++ HW/3 les/task2/index.js
@@ -0,0 +1,11 @@
+let age = 24;
+age = '24';
+alert(age);
+
+let firstName = 'Matvey';
+firstName = 101010;
+alert(firstName);
+
+let blackIsAFavoriteColour = false;
+blackIsAFavoriteColour = null;
+alert(blackIsAFavoriteColour);
