62542546991bd3a714df41a97eebe0b6a41ecf7b
Mainecolog <123167632+Mainecolog@users.noreply.github.com>
2023-01-24 13:41:29 +0300

    lesson 4 done

---
+++ HW/4 les/task2/task-2.js
@@ -0,0 +1,21 @@
+console.log(Number('10'), typeof Number('10')); // явное
+//1
+console.log(Number('50'),typeof Number('50')); 
+//2
+console.log(Number('100')); // явное
+//3
+console.log(Number(''), typeof Number('')); 
+//4
+console.log(String(1),typeof Number(1)); // явное
+//5
+console.log(Boolean(0),typeof Number(0)); // явное
+//6
+console.log(Number('001'), typeof Number('001')); 
+//7
+console.log(Number(''), typeof Number('')); 
+//8
+console.log(Boolean(1),typeof Number(1)); // явное
+//9
+console.log(String(001), typeof Number (001)); // явное
+//10
+console.log(Number('Hello World'), typeof Number('Hello World')); // явное 
